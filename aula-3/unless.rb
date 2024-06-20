41a60a47e6370ac291700d1ede7d02c8f1350ca1
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-06-20 18:25:40 -0300

    Create unless.rb

---
+++ aula-3/unless.rb
@@ -0,0 +1,12 @@
+# usar o unless somente quando a condição negativa deixar o código mais claro.
+
+
+age = 17
+
+unless age < 18
+    puts "Você é adulto"
+else
+    puts "Você é menor de idade"
+end
+
+
