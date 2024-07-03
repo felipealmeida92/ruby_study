614af1a76a3cef93ee4a52aedd67ac620c5e7a2f
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-07-03 19:22:33 -0300

    Update atributos.rb

--- aula-7 POO Parte II/atributos.rb
+++ aula-7 POO Parte II/atributos.rb
@@ -1,14 +1,10 @@
 class Dog
-
-    def name
-        @name
-    end
-
-    def name= name
-        @name = name
-    end
+    attr_accessor :name, :age
 end
 
 dog = Dog.new
 dog.name = 'Marlon'
 puts dog.name
+
+dog.age = '1 ano'
+puts dog.age
