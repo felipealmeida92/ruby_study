3b562d7fbc21d9a39c23119bd4e191898502fd67
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-07-05 20:55:46 -0300

    Update block.rb

--- aula-8 Ruby avancado/block.rb
+++ aula-8 Ruby avancado/block.rb
@@ -13,3 +13,46 @@ foo.each do |key, value|
   puts "key * value = #{key * value}"
   puts '---'
 end
+
+def foo
+  # Call the block
+  yield
+  yield
+end
+
+foo { puts "Exec the block" }
+
+def foo
+  if block_given?
+    # Call the block
+    yield
+  else
+    puts "Sem parâmetro do tipo bloco"
+  end
+end
+
+foo
+foo { puts "Com parâmetro do tipo bloco" }
+
+def foo(name, &block)
+  @name = name
+  block.call
+end
+
+foo ('Felipe') { puts "Hellow #{@name}" }
+
+def foo(numbers, &block)
+  if block_given?
+    numbers.each do |key, value|
+      block.call(key, value)
+    end
+  end
+end
+
+numbers = { 2 => 2, 3 => 3, 4 => 4 }
+
+foo(numbers) do |key, value|
+  puts "#{key} * #{value} = #{key * value}"
+  puts "#{key} + #{value} = #{key + value}"
+  puts "---"
+end
