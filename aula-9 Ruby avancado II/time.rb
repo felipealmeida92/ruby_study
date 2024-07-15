7b12eea9f110682bc105c5a57b823a6dd3537bce
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-07-15 18:33:28 -0300

    Create time.rb

---
+++ aula-9 Ruby avancado II/time.rb
@@ -0,0 +1,20 @@
+time = Time.now
+
+# puts time
+
+
+# puts time.year
+
+# puts time.month
+
+# puts time.day
+
+# puts time.strftime('%Y-%m-%d %H:%M:%S UTC')
+
+# puts time.monday?
+
+time2 = Time.now
+
+# puts time == time2 # => false
+
+puts time.year == time2.year # => true
